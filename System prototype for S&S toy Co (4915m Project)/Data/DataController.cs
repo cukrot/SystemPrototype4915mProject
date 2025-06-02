048548dca715b9be747612deca8b3e37b6422288
cukrot <60045940+cukrot@users.noreply.github.com>
2025-06-02 14:14:58 +0800

    test

---
+++ System prototype for S&S toy Co (4915m Project)/Data/DataController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System_prototype_for_S_S_toy_Co__4915m_Project_.Data
+{
+    internal class DataController
+    {
+    }
+}
---
+++ System prototype for S&S toy Co (4915m Project)/Data/ViewXXX.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace System_prototype_for_S_S_toy_Co__4915m_Project_.Data
+{
+    public partial class ViewXXX : Form
+    {
+        public ViewXXX()
+        {
+            InitializeComponent();
+        }
+    }
+}
