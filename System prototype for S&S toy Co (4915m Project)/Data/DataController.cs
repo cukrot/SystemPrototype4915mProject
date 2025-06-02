fd17050b07db5888e00edfb5c83c8ef1890c243d
cukrot <60045940+cukrot@users.noreply.github.com>
2025-06-02 14:25:45 +0800

    test2

--- System prototype for S&S toy Co (4915m Project)/Data/DataController.cs
+++ System prototype for S&S toy Co (4915m Project)/Data/DataController.cs
@@ -1,12 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace System_prototype_for_S_S_toy_Co__4915m_Project_.Data
 {
-    internal class DataController
+    internal class DataController : SubSystemController
     {
+        public async Task<DataTable> GetData()
+        {
+            DataTable dt = null;
+            try
+            {
+                dt = await GetTableData("customers"); //specify table name
+                //Or you can use GetData()
+                //dt = await GetData("GetTableData", "customers"); //specify endpoint & table name
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return dt;
+        }
     }
 }
--- System prototype for S&S toy Co (4915m Project)/Data/ViewXXX.cs
+++ System prototype for S&S toy Co (4915m Project)/Data/ViewXXX.cs
@@ -7,14 +7,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System_prototype_for_S_S_toy_Co__4915m_Project_.JustTesting;
 
 namespace System_prototype_for_S_S_toy_Co__4915m_Project_.Data
 {
+
+
     public partial class ViewXXX : Form
     {
+        DataController controll;
         public ViewXXX()
         {
             InitializeComponent();
+        }
+
+        private async void ViewXXX_Load(object sender, EventArgs e)
+        {
+            controll = new DataController();
+
+            try
+            {
+                DataTable dt = await controll.GetData();
+                dataGridView1.DataSource = dt;
+                dt.AcceptChanges();
+            }
+            catch (Exception ex)
+            {
+                // Log any other exceptions
+                MessageBox.Show($"An error occurred: {ex.Message}");
+            }
         }
     }
 }
